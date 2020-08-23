73efbda5cdd94f4c58de9764e51e8a9206221308
Luke Wilson <l2wilson94@gmail.com>
2020-08-23 14:44:53 +0100

    initial upload

---
+++ make.bat
@@ -0,0 +1,1 @@
+deno run --allow-all make.js
---
+++ make.js
@@ -0,0 +1,31 @@
+
+const decoder = new TextDecoder("utf-8")
+const encoder = new TextEncoder("utf-8")
+
+let source = ""
+
+const readDir = async (path) => {
+	for await (const dirEntry of Deno.readDir(path)) {
+		const entryPath = `${path}/${dirEntry.name}`
+		if (dirEntry.isDirectory) await readDir(entryPath)
+		else await readFile(entryPath)
+	}
+}
+
+const readFile = async (path) => {
+	const fileData = await Deno.readFile(path)
+	const fileSource = decoder.decode(fileData)
+	source += `\n\n//==== ${path} ====//\n;`
+	source += fileSource
+}
+
+await readFile("Source/EventWindow.js")
+await readFile("Source/Symmetry.js")
+await readFile("Source/Element.js")
+await readFile("Source/Instruction.js")
+await readFile("Source/Javascript.js")
+await readFile("Source/Parse.js")
+
+const data = encoder.encode(source)
+await Deno.writeFile("Build/SpaceTode.js", data)
+console.log("[SpaceTode] Build finished")
